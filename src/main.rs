7689b8c392caf45a10a160364856524e178e6cba
charlesndalton <charlesndalton@gmail.com>
2022-07-02 09:14:49 -0700

    feat: basic structural setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use anyhow::{Result, Context};
+
+fn main() -> Result<()> {
+    let telegram_token = env::var("ORYX_TELEGRAM_TOKEN").expect("ORYX_TELEGRAM_TOKEN not set");
+    let infura_api_key = env::var("INFURA_API_KEY").expect("INFURA_API_KEY not set");
+
+    report_creator::create_report()?;
+
+    report_publisher::publish_report()?;
+
+    Ok(())
+}
+
+mod report_creator {
+    use super::*;
+
+    pub fn create_report() -> Result<()> {
+        Ok(())
+    }
+}
+
+mod report_publisher {
+    use super::*;
+
+    pub fn publish_report() -> Result<()> {
+        Ok(())
+    }
 }
