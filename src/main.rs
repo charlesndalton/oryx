eaf436588a11f515661197ef0f657b9abe5fa8e4
charlesndalton <charlesndalton@gmail.com>
2022-07-31 11:41:49 -0700

    feat: add USDT strategy

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use anyhow::{Context, Result};
+use bigdecimal::{BigDecimal, FromPrimitive, ToPrimitive};
+use derive_getters::Getters;
+use derive_new::new;
+use log::{error, info};
 use std::env;
-use anyhow::{Result, Context};
-use bigdecimal::{BigDecimal, ToPrimitive, FromPrimitive};
 use std::str::FromStr;
-use derive_getters::{Getters};
-use derive_new::{new};
-use log::{info, error};
 use std::sync::Arc;
 
-const STARGATE_STRATEGY_ADDRESSES: [&'static str; 1] = ["0x7C85c0a8E2a45EefF98A10b6037f70daf714B7cf"];
+const STARGATE_STRATEGY_ADDRESSES: [&'static str; 2] = [
+    "0x7C85c0a8E2a45EefF98A10b6037f70daf714B7cf",
+    "0xeAD650E673F497CdBE365F7a855273BbB468e454",
+];
 
 #[derive(new, Getters, Debug)]
 pub struct StargateReport {
-    individual_strategy_reports: Vec<IndividualStrategyReport>
+    individual_strategy_reports: Vec<IndividualStrategyReport>,
 }
 
 #[derive(new, Getters, Debug)]
@@ -32,9 +35,13 @@ async fn main() -> Result<()> {
 
     info!("================== ORYX RUNNING ==================");
 
-    let report = report_creator::create_report(infura_api_key).await.context("Failed to create report")?;
+    let report = report_creator::create_report(infura_api_key)
+        .await
+        .context("Failed to create report")?;
 
-    report_publisher::publish_report(report, telegram_token).await.context("Failed to publish report")?;
+    report_publisher::publish_report(report, telegram_token)
+        .await
+        .context("Failed to publish report")?;
 
     Ok(())
 }
@@ -44,17 +51,30 @@ mod report_publisher {
     use num_format::{Locale, ToFormattedString};
 
     pub async fn publish_report(report: StargateReport, telegram_token: String) -> Result<()> {
-        let mut report_formatted = vec!(
-            String::from("Daily Stargate Report"),
-        );
+        let mut report_formatted = vec![String::from("Daily Stargate Report")];
 
         for individual_strategy_report in report.individual_strategy_reports() {
             report_formatted.push(String::from("--------------"));
-            report_formatted.push(format!("Asset – {}", &individual_strategy_report.asset_name())); 
-            report_formatted.push(format!("Yearn Strategy TVL: ${}", prettify(&individual_strategy_report.strategy_tvl())));
-            report_formatted.push(format!("Pool Current Ratio: {}", individual_strategy_report.current_ratio().with_scale(2)));
-            report_formatted.push(format!("Total Pool Liabilities: ${}", prettify(&individual_strategy_report.pool_liabilities())));
-            report_formatted.push(format!("Total Pool Liquidity: ${}", prettify(&individual_strategy_report.pool_liquidity())));
+            report_formatted.push(format!(
+                "Asset – {}",
+                &individual_strategy_report.asset_name()
+            ));
+            report_formatted.push(format!(
+                "Yearn Strategy TVL: ${}",
+                prettify(&individual_strategy_report.strategy_tvl())
+            ));
+            report_formatted.push(format!(
+                "Pool Current Ratio: {}",
+                individual_strategy_report.current_ratio().with_scale(2)
+            ));
+            report_formatted.push(format!(
+                "Total Pool Liabilities: ${}",
+                prettify(&individual_strategy_report.pool_liabilities())
+            ));
+            report_formatted.push(format!(
+                "Total Pool Liquidity: ${}",
+                prettify(&individual_strategy_report.pool_liquidity())
+            ));
         }
 
         let report_for_telegram = report_formatted.join("\n");
@@ -76,13 +96,17 @@ mod telegram_client {
     use super::*;
     use urlencoding::encode;
 
-    const STARGATE_COMMITTEE_TELEGRAM_CHAT_ID: i64 = -753837580; 
+    const STARGATE_COMMITTEE_TELEGRAM_CHAT_ID: i64 = -753837580;
 
     pub async fn send_message_to_committee(message: &str, token: &str) -> Result<()> {
-        let url = format!("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}", token, STARGATE_COMMITTEE_TELEGRAM_CHAT_ID, encode(message));
+        let url = format!(
+            "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}",
+            token,
+            STARGATE_COMMITTEE_TELEGRAM_CHAT_ID,
+            encode(message)
+        );
 
-        reqwest::get(url)
-            .await?;
+        reqwest::get(url).await?;
 
         Ok(())
     }
@@ -90,8 +114,8 @@ mod telegram_client {
 
 mod report_creator {
     use super::*;
-    use ethers::abi::Address;
     use crate::blockchain_client::{StargateStrategy, IERC20};
+    use ethers::abi::Address;
 
     pub async fn create_report(infura_api_key: String) -> Result<StargateReport> {
         let mut individual_strategy_reports = Vec::new();
@@ -100,7 +124,7 @@ mod report_creator {
         for strategy_address in STARGATE_STRATEGY_ADDRESSES {
             let strategy = StargateStrategy::new(
                 strategy_address.parse::<Address>()?,
-                Arc::clone(&eth_client)
+                Arc::clone(&eth_client),
             );
 
             let want = strategy.get_want(Arc::clone(&eth_client)).await?;
@@ -113,35 +137,38 @@ mod report_creator {
 
             info!("Strategy decimals: {}", decimals);
 
-            let total_position = (strategy.get_total_position().await? / 10_i64.pow(decimals)).with_scale(0);
+            let total_position =
+                (strategy.get_total_position().await? / 10_i64.pow(decimals)).with_scale(0);
 
-            info!("Total position: ${}", total_position); 
+            info!("Total position: ${}", total_position);
 
             let liquidity_pool_address = strategy.get_underlying_liquidity_pool().await?;
 
             info!("Pool address: {}", liquidity_pool_address);
 
-            let total_liquidity = (want.balance_of(liquidity_pool_address).await? / 10_i64.pow(decimals)).with_scale(0);
+            let total_liquidity = (want.balance_of(liquidity_pool_address).await?
+                / 10_i64.pow(decimals))
+            .with_scale(0);
 
             info!("Liquidity: {}", total_liquidity);
 
             let pool_token = IERC20::new(liquidity_pool_address, Arc::clone(&eth_client));
 
-            let pool_liabilities = (pool_token.total_supply().await? / 10_i64.pow(decimals)).with_scale(0);
+            let pool_liabilities =
+                (pool_token.total_supply().await? / 10_i64.pow(decimals)).with_scale(0);
 
             info!("Pool liabilities: {}", pool_liabilities);
 
             let current_ratio = &total_liquidity / &pool_liabilities;
 
             individual_strategy_reports.push(IndividualStrategyReport::new(
-                    symbol, 
-                    total_position,
-                    total_liquidity,
-                    pool_liabilities,
-                    current_ratio
-                ));
+                symbol,
+                total_position,
+                total_liquidity,
+                pool_liabilities,
+                current_ratio,
+            ));
         }
-
 
         Ok(StargateReport::new(individual_strategy_reports))
     }
@@ -151,12 +178,9 @@ mod blockchain_client {
     use super::*;
     use ethers::prelude::*;
 
-    abigen!(
-        UnwrappedIERC20,
-        "./src/abis/IERC20.json"
-    );
+    abigen!(UnwrappedIERC20, "./src/abis/IERC20.json");
     pub struct IERC20 {
-        instance: UnwrappedIERC20<Provider::<Http>> // holds the ethers object so we don't need to re-create it a bunch
+        instance: UnwrappedIERC20<Provider<Http>>, // holds the ethers object so we don't need to re-create it a bunch
     }
 
     abigen!(
@@ -164,11 +188,11 @@ mod blockchain_client {
         "./src/abis/StargateStrategy.json"
     );
     pub struct StargateStrategy {
-        instance: UnwrappedStargateStrategy<Provider::<Http>>
+        instance: UnwrappedStargateStrategy<Provider<Http>>,
     }
-    //pub type StargateStrategy = UnwrappedStargateStrategy<Provider::<Http>>; 
+    //pub type StargateStrategy = UnwrappedStargateStrategy<Provider::<Http>>;
 
-    pub type Client = Arc<Provider::<Http>>;
+    pub type Client = Arc<Provider<Http>>;
 
     pub fn create_client(infura_api_key: &str) -> Result<Client> {
         let infura_url = format!("https://mainnet.infura.io/v3/{}", infura_api_key);
@@ -178,7 +202,9 @@ mod blockchain_client {
 
     impl IERC20 {
         pub fn new(token_address: Address, client: Client) -> Self {
-            Self { instance: UnwrappedIERC20::new(token_address, client) }
+            Self {
+                instance: UnwrappedIERC20::new(token_address, client),
+            }
         }
 
         pub async fn decimals(&self) -> Result<u32> {
@@ -205,12 +231,14 @@ mod blockchain_client {
 
     impl StargateStrategy {
         pub fn new(strategy_address: Address, client: Client) -> Self {
-            Self { instance: UnwrappedStargateStrategy::new(strategy_address, client) }
+            Self {
+                instance: UnwrappedStargateStrategy::new(strategy_address, client),
+            }
         }
 
         pub async fn get_want(&self, client: Client) -> Result<IERC20> {
             let want_address = self.instance.want().call().await?;
-            let want = IERC20::new(want_address, client); 
+            let want = IERC20::new(want_address, client);
 
             Ok(want)
         }
@@ -225,8 +253,4 @@ mod blockchain_client {
             Ok(self.instance.liquidity_pool().call().await?)
         }
     }
-
 }
-
-
-
